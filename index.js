bd98b95ebd63a34f96884f8840b5c7c79e79cf4f
girlwhocodes01 <40805554+girlwhocodes01@users.noreply.github.com>
2018-07-03 15:19:00 -0400

    create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var imgID;
+imgID = 1
+alert(imgID);
+
+
+
+function changePicture(){
+  // img=document.getElementById("sf");
+// //   if imgID == 0 {
+// //     document.getElementById("scienceFiction").innerHTML = 'img src="ScytheCover.jpg" alt="Scythe cover" style="width:200px;height:300px;"'
+// //     alert(imgID);
+// //   }
+// // //     imgID +=1
+  if imgID == 1 {
+    document.getElementById("sf").innerHTML = 'img src="UnwindCover.jpg" alt="Unwind cover" style="width:200px;height:300px;"'
+    // alert(imgID);
+  }
+//       }if imgID == 2 {
+//     img.innerhtml = 'img src="WarcrossCover.jpg" alt="Warcross cover" style="width:200px;height:300px;"'
+//     alert(imgID);
+//     imgID = 0
+//   }
+//
+// }
